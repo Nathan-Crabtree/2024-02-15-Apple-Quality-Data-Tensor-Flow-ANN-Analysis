af8bbd566e5526e91d366a56bac4798bca6a3c40
Nathan Crabtree <nathanielmarkcrabtree@gmail.com>
2024-02-15 15:50:22 -0600

    second commit

--- analysis.py
+++ analysis.py
@@ -1,3 +1,5 @@
+#data available here: https://www.kaggle.com/datasets/nelgiriyewithana/apple-quality
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
